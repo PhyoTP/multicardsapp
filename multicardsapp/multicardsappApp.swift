27fa61dbaf413d7b028a20073c7cb1a23949a51e
PhyoTP <phyo.thet.pai@sji.edu.sg>
2025-11-10 22:01:27 +0800

    Initial Commit

---
+++ multicardsapp/multicardsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  multicardsappApp.swift
+//  multicardsapp
+//
+//  Created by Phyo Thet Pai on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct multicardsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
